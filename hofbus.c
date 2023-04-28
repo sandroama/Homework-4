6ae5181b4655fa78aae9125955f3679043a4ded6
SandroAma <sandroama123@gmail.com>
2023-04-27 21:44:36 -0400

    abc

--- hofbus.c
+++ hofbus.c
@@ -47,8 +47,6 @@ station_load_bus(struct station *station, int count)
     pthread_mutex_unlock(&station->mutex);
 }
 
-
-
 int
 station_wait_for_bus(struct station *station, int myticket, int myid)
 {
@@ -56,6 +54,10 @@ station_wait_for_bus(struct station *station, int myticket, int myid)
     station->waiting_students++;
 
     while (myticket != station->boarding_turn + 1 || station->free_seats <= 0) {
+        if (station->free_seats <= 0) {
+            pthread_mutex_unlock(&station->mutex);
+            return 0;
+        }
         pthread_cond_wait(&station->bus_arrived, &station->mutex);
     }
 
@@ -67,7 +69,7 @@ station_wait_for_bus(struct station *station, int myticket, int myid)
     printf("student %d with ticket %d has boarded, the turn is %d\n", myid, myticket, station->boarding_turn);
 
     pthread_cond_signal(&station->student_boarded);
-    
+
     if (station->free_seats == 0 || station->waiting_students == 0) {
         pthread_cond_broadcast(&station->all_boarded); // added this line
     }
@@ -76,3 +78,32 @@ station_wait_for_bus(struct station *station, int myticket, int myid)
 
     return station->boarding_turn;
 }
+
+
+// int
+// station_wait_for_bus(struct station *station, int myticket, int myid)
+// {
+//     pthread_mutex_lock(&station->mutex);
+//     station->waiting_students++;
+
+//     while (myticket != station->boarding_turn + 1 || station->free_seats <= 0) {
+//         pthread_cond_wait(&station->bus_arrived, &station->mutex);
+//     }
+
+//     station->boarding_turn++;
+//     station->waiting_students--;
+//     station->free_seats--;
+//     station->boarded_students++;
+
+//     printf("student %d with ticket %d has boarded, the turn is %d\n", myid, myticket, station->boarding_turn);
+
+//     pthread_cond_signal(&station->student_boarded);
+    
+//     if (station->free_seats == 0 || station->waiting_students == 0) {
+//         pthread_cond_broadcast(&station->all_boarded); // added this line
+//     }
+    
+//     pthread_mutex_unlock(&station->mutex);
+
+//     return station->boarding_turn;
+// }
